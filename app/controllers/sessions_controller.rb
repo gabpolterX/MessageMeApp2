ce2d5becfcd8efa8b6c4e25262806b4709ad8ed0
Gabriel Castro <gabocastrosemper28@gmail.com>
2024-12-20 11:45:32 -0500

    Log in sessions working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,21 @@ class SessionsController < ApplicationController
     end
 
     def create
-
+        user = User.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:sucess] = "Logged in succesfully"
+            redirect_to root_path
+        else
+            flash.now[:error] = "There was something wrong"
+            render 'new'
+        end
     end
     
     def destroy
-
+        session[:user_id] = nil
+        flash[:notice] = "You logged out"
+        redirect_to login_path
     end
 
 
